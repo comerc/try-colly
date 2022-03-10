79974549e94680b15159b2ac019d489e16befce6
comerc <andrew.kachanov@gmail.com>
2022-03-10 05:27:46 +0300

    Release

--- main.go
+++ main.go
@@ -2,54 +2,161 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path"
+	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
 )
 
-func main() {
-	c := colly.NewCollector(
-		// MaxDepth is 1, so only the links on the scraped page
-		// is visited, and no further links are followed
-		colly.MaxDepth(1),
+// Cases:
+// + http://e38.ru/dir1/dir2/ // запишу index.html в папку /dir1/dir2/ // TODO: не работает
+// + http://e38.ru/dir1/dir2/file // запишу file в папку /dir1/dir2/
+// + http://e38.ru/shoutbox?page=50 // надо парсить каждый url
+// + http://e38.ru/node/3474#comment-61752 // отсекаю до /node/3474
+// + http://e38.ru/node/37019?scroll&all#comments // отсекаю до /node/37019
+// + http://e38.ru/user/login?destination=node/3474%2523comment-form // игнорирую
+// + http://e38.ru/user/register?destination=node/37113%2523comment-form // игнорирую
+// + http://e38.ru/files/* // игнорирую
 
-		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("go-colly.org"),
+func main() {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	argsWithProg := os.Args
+	if len(argsWithProg) != 2 {
+		fmt.Print("need domain")
+		os.Exit(1)
+	}
+	domain := argsWithProg[1]
+
+	log.SetFlags(log.LUTC | log.Ldate | log.Ltime | log.Lshortfile)
+	logFile, err := os.OpenFile(path.Join(currentPath, "log_"+domain+".log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
+	visitedFilePath := path.Join(currentPath, "visited_"+domain)
+	var visited []string
+	if _, err := os.Stat(visitedFilePath); err == nil {
+		data, err := os.ReadFile(visitedFilePath)
+		if err != nil {
+			log.Panicf("ERROR: %s", err)
+		}
+		visited = strings.Split(string(data), "\n")
+	}
+	visitedFile, err := os.OpenFile(visitedFilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		log.Printf("ERROR: %s", err)
+	}
+	defer visitedFile.Close()
+
+	baseDir := path.Join(currentPath, "sites", domain)
+
+	c := colly.NewCollector(
+		colly.MaxDepth(1),
+		colly.AllowedDomains(domain),
 	)
 	c.SetRequestTimeout(120 * time.Second)
+	c.IgnoreRobotsTxt = false
 
-	// Find and visit all links
+	c.OnRequest(func(r *colly.Request) {
+		log.Printf("Visit to %s\n", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		contentType := strings.Split(r.Headers.Get("Content-Type"), ";")[0]
+		if contentType != "text/html" {
+			return
+		}
+		urlPath := r.Request.URL.Path
+		filePath := strings.Split(urlPath, "/")
+		dirPath := strings.Join(filePath[:len(filePath)-1], "/")
+		fileName := filePath[len(filePath)-1]
+		urlQuery := r.Request.URL.RawQuery
+		if urlQuery != "" && substr(urlQuery, 0, len("page")) == "page" {
+			fileName = fileName + "?" + urlQuery
+		}
+		if fileName == "" {
+			fileName = "index"
+		}
+		fileName = fileName + ".html"
+		{
+			err := os.MkdirAll(path.Join(baseDir, dirPath), 0755)
+			if err != nil {
+				log.Printf("ERROR: %s", err)
+			}
+		}
+		{
+			err := os.WriteFile(path.Join(baseDir, dirPath, fileName), r.Body, 0644)
+			if err != nil {
+				log.Printf("ERROR: %s", err)
+			}
+		}
+		log.Printf("Saved to %s", path.Join(dirPath, fileName))
+	})
+
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		// e.Request.Visit(e.Attr("href"))
-
-		// link := e.Attr("href")
-		// // Print link
-		// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// // Visit link found on page
-		// // Only those links are visited which are in AllowedDomains
-		// c.Visit(e.Request.AbsoluteURL(link))
-
 		link := e.Request.AbsoluteURL(e.Attr("href"))
-		if link != "" {
-			fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-			c.Visit(link)
+		URL, err := url.Parse(link)
+		if err != nil {
+			log.Printf("ERROR: %s", err)
+		}
+		urlPath := URL.Path
+		if urlPath == "/user/login" || urlPath == "/user/register" || substr(urlPath, 0, len("/files/")) == "/files/" {
+			return
+		}
+		shortLink := urlPath
+		urlQuery := URL.RawQuery
+		if urlQuery != "" && substr(urlQuery, 0, len("page")) == "page" {
+			shortLink = shortLink + "?" + urlQuery
+		}
+		if indexOf(visited, shortLink) == -1 {
+			_, err := visitedFile.WriteString(shortLink + "\n")
+			if err != nil {
+				log.Printf("ERROR: %s", err)
+			}
+			visitedFile.Sync()
+			visited = append(visited, shortLink)
+			c.Visit(link) // !!! link (а не shortLink)
 		}
 	})
 
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Printf("Visiting %s\n", r.URL)
-	// })
-
-	// c.OnResponse(func(r *colly.Response) {
-
-	// 	fmt.Printf("Visited ContentType: %s FileName: %s\n", r.Headers.Get("Content-Type"), r.FileName())
-	// })
-
-	// c.OnScraped(func(r *colly.Response) {})
-
-	c.OnError(func(r *colly.Response, e error) {
-		fmt.Println("Got this error:", e)
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("ERROR: %s", err)
 	})
 
-	c.Visit("http://go-colly.org/")
+	c.Visit(fmt.Sprintf("http://%s/", domain))
+}
+
+func indexOf(slice []string, item string) int {
+	for i := range slice {
+		if slice[i] == item {
+			return i
+		}
+	}
+	return -1
+}
+
+func substr(input string, start int, length int) string {
+	asRunes := []rune(input)
+
+	if start >= len(asRunes) {
+		return ""
+	}
+
+	if start+length > len(asRunes) {
+		length = len(asRunes) - start
+	}
+
+	return string(asRunes[start : start+length])
 }
